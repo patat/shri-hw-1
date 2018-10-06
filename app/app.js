cc69f1034f8b84a9d9e95f97fbce7cb880d0209f
Patat <nemnogosloven@gmail.com>
2018-10-07 02:01:29 +0300

    task 2 start

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import mainMenu from './main-menu.js';
 import lineClamp from './line-clamp.js';
 import render from './render.js';
+import moveCamera from './move-camera.js';
 
 export default function app() {
   document.addEventListener('DOMContentLoaded', function() {
@@ -10,11 +11,14 @@ export default function app() {
       .then(response => response.json())
       .then(data => render(data.events));
 
-    // make adding ellipsis async to pick up DOM changes
+    // make further DOM munipulations async to pick up DOM changes
     setTimeout(() => {
       const cardHeadingsToClamp = document.querySelectorAll('.line-clamp');
       cardHeadingsToClamp.forEach(cardHeading => lineClamp(cardHeading));
-    }, 0);
+
+      const camera = document.querySelector('.event-card__camera');
+      moveCamera(camera);
+    }, 10);
     
   });
 };
