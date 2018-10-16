344ba430d2beb0739002248938ad86faef66af35
Patat <nemnogosloven@gmail.com>
2018-10-16 06:41:46 +0300

    refactor cam-app to encapsulate logic in CamVideo class

--- app/camera/WidgetSlider.js
+++ app/camera/WidgetSlider.js
@@ -1,14 +1,15 @@
 /* global requestAnimationFrame */
 export default class WidgetSlider {
-  constructor (el, callback, initialValue = 0.5) {
+  constructor (el, initialValue = 0.5) {
     this.sliderCursor = el.querySelector('.slider__cursor');
     this.isVertical = false;
 
     this.sliderRect = el.getBoundingClientRect();
-    this.callback = callback;
+
     this.minDisplacement = 0;
     this.maxDisplacement = this.sliderRect.width - this.sliderCursor.offsetWidth;
 
+    // slider value is always in [0, 1] interval
     this.cursorDisplacement = Math.floor(this.maxDisplacement * initialValue);
     this.sliderCursor.style.left = this.cursorDisplacement + 'px';
     this.tracking = false;
@@ -17,20 +18,32 @@ export default class WidgetSlider {
     this.sliderCursor.addEventListener('pointermove', ev => this.sliderMove(ev), true);
     this.sliderCursor.addEventListener('pointerup', ev => this.sliderEnd(ev), true);
     this.sliderCursor.addEventListener('pointercancel', ev => this.sliderEnd(ev), true);
-    callback(this.getValue());
   }
 
   getValue () {
     return this.cursorDisplacement / this.maxDisplacement;
   }
 
+  setValue (value) {
+    let newValue = value;
+    if (newValue < 0) {
+      newValue = 0;
+    }
+    if (newValue > 1) {
+      newValue = 1;
+    }
+
+    this.cursorDisplacement = Math.floor(this.maxDisplacement * newValue);
+    this.sliderCursor.style.left = this.cursorDisplacement + 'px';
+  }
+
+  bindCallback (callback) {
+    this.callback = callback;
+  }
+
   sliderStart (ev) {
     if (!this.tracking) {
       this.sliderCursor.setPointerCapture(ev.pointerId);
-      const cursorDisplacement = Number.parseInt(this.sliderCursor.style.left, 10);
-      if (!Number.isNaN(cursorDisplacement)) {
-        this.cursorDisplacement = cursorDisplacement;
-      }
 
       this.prevCoord = this.isVertical ? ev.clientY : ev.clientX;
       this.tracking = true;
--- app/camera/cam-app.js
+++ app/camera/cam-app.js
@@ -1,27 +1,10 @@
-/* global Hls requestAnimationFrame */
 import mainMenu from '../main-menu.js';
 import WidgetSlider from './WidgetSlider.js';
+import CamVideo from './CamVideo.js';
 
 export default function () {
   document.addEventListener('DOMContentLoaded', function () {
     mainMenu();
-    console.log('it works!');
-
-    function initVideo (video, url) {
-      if (Hls.isSupported()) {
-        var hls = new Hls();
-        hls.loadSource(url);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, function () {
-          video.play();
-        });
-      } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
-        video.addEventListener('loadedmetadata', function () {
-          video.play();
-        });
-      }
-    }
 
     const videos = [
       {
@@ -43,105 +26,43 @@ export default function () {
     ];
 
     const state = {
-      videos: [],
-      isFullscreen: false,
-      currentVideo: null,
-      currentVideoIndex: null,
-      brightness: [],
-      contrast: [],
-      analysers: []
+      camVideos: []
     };
 
     const fullscreenVideoBG = document.querySelector('.fullscreen-video__bg');
     const fullscreenVideoControls = document.querySelector('.fullscreen-video__controls');
     const goBackBtn = fullscreenVideoControls.querySelector('.fullscreen-video__back-btn');
-    const volumeLevel = fullscreenVideoControls.querySelector('.fullscreen-video__volume-level');
+    const volumeWidget = fullscreenVideoControls.querySelector('.fullscreen-video__volume-level');
+    const sliderBrightnessEl = document.getElementById('slider-brightness');
+    const sliderBrightness = new WidgetSlider(sliderBrightnessEl);
+    const sliderContrastEl = document.getElementById('slider-contrast');
+    const sliderContrast = new WidgetSlider(sliderContrastEl);
 
     videos.forEach((video, index) => {
-      const videoEl = document.getElementById(video.id);
-      initVideo(videoEl, video.url);
+      const camVideo = new CamVideo(Object.assign({}, video, {
+        bg: fullscreenVideoBG,
+        controlPanel: fullscreenVideoControls,
+        volumeWidget: volumeWidget,
+        backBtn: goBackBtn,
+        brightnessWidget: sliderBrightness,
+        contrastWidget: sliderContrast
+      }));
 
-      const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-      const analyser = audioCtx.createAnalyser();
-      const source = audioCtx.createMediaElementSource(videoEl);
-      source.connect(analyser);
-      analyser.connect(audioCtx.destination);
-      analyser.fftSize = 32;
+      const audioAnalizer = createAudioAnalizer(camVideo.el);
+      camVideo.setAudioAnalizer(audioAnalizer);
 
-      state.videos.push(videoEl);
-      state.analysers.push(analyser);
-      state.brightness.push(1);
-      state.contrast.push(1);
-
-      videoEl.addEventListener('click', expandToFullscreen);
+      state.camVideos.push(camVideo);
     });
-
-    goBackBtn.addEventListener('click', cancelFullscreen);
-
-    function expandToFullscreen (ev) {
-      const elRect = ev.target.getBoundingClientRect();
-
-      const elRatio = elRect.width / elRect.height;
-      const viewportRatio = window.innerWidth / window.innerHeight;
-      const scaleX = window.innerWidth / elRect.width;
-      const scaleY = window.innerHeight / elRect.height;
-      const scale = elRatio >= viewportRatio ? scaleX : scaleY;
-      const translateX = (window.innerWidth - elRect.width * scale) / 2 - elRect.left;
-      const translateY = (window.innerHeight - elRect.height * scale) / 2 - elRect.top;
-
-      if (!state.isFullscreen) {
-        state.currentVideo = ev.target;
-        state.currentVideoIndex = state.videos.findIndex(video => video === ev.target);
-        ev.target.parentNode.style = `transform: translate(${translateX}px, ${translateY}px) scale(${scale}); z-index: 2005;`;
-        fullscreenVideoBG.style = `z-index: 2000; opacity: 1`;
-
-        setTimeout(() => {
-          fullscreenVideoControls.style = `z-index: 2010; opacity: 1`;
-        }, 500);
-
-        const sliderBrightnessEl = document.getElementById('slider-brightness');
-        const sliderBrightness = new WidgetSlider(sliderBrightnessEl, function (value) {
-          state.brightness[state.currentVideoIndex] = value * 2;
-          state.currentVideo.style.filter = `brightness(${state.brightness[state.currentVideoIndex]}) contrast(${state.contrast[state.currentVideoIndex]})`;
-        }, state.brightness[state.currentVideoIndex] / 2);
-
-        const sliderContrastEl = document.getElementById('slider-contrast');
-        const sliderContrast = new WidgetSlider(sliderContrastEl, function (value) {
-          state.contrast[state.currentVideoIndex] = value * 2;
-          state.currentVideo.style.filter = `brightness(${state.brightness[state.currentVideoIndex]}) contrast(${state.contrast[state.currentVideoIndex]})`;
-        }, state.contrast[state.currentVideoIndex] / 2);
-
-        state.currentVideo.muted = false;
-        const analyser = state.analysers[state.currentVideoIndex];
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-
-        const visualizeVolume = function () {
-          analyser.getByteFrequencyData(dataArray);
-          const peakFrequency = Math.max.apply(null, dataArray);
-
-          const volumeLevelPercent = Math.floor(100 * peakFrequency / 255);
-          volumeLevel.style.transform = `translateY(${100 - volumeLevelPercent}%)`;
-
-          if (state.currentVideo) {
-            requestAnimationFrame(visualizeVolume);
-          }
-        };
-
-        visualizeVolume();
-
-        state.isFullscreen = true;
-      }
-    }
-
-    function cancelFullscreen (ev) {
-      state.currentVideo.parentNode.style = `transform: scale(1)`;
-      fullscreenVideoBG.style = `z-index: -1; opacity: 0`;
-      fullscreenVideoControls.style = `z-index: -1; opacity: 0`;
-      state.currentVideo.muted = true;
-      state.currentVideo = null;
-      state.currentVideoIndex = null;
-      state.isFullscreen = false;
-    }
   });
+}
+
+function createAudioAnalizer (mediaEl) {
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  const analyser = audioCtx.createAnalyser();
+  const source = audioCtx.createMediaElementSource(mediaEl);
+  source.connect(analyser);
+  analyser.connect(audioCtx.destination);
+  analyser.fftSize = 32;
+
+  return analyser;
 }
