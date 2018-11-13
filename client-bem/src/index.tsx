0762702f434cccf1a0a74308cb0931131b20f5d3
Patat <nemnogosloven@gmail.com>
2018-11-13 23:41:39 +0300

    mvp

--- client-bem/src/index.tsx
+++ client-bem/src/index.tsx
@@ -4,8 +4,14 @@ import './index.css';
 import { App as AppDesktop } from './desktop.blocks/App/App';
 import { App as AppMobile } from './mobile.blocks/App/App';
 import * as serviceWorker from './serviceWorker';
+import MobileDetect from 'mobile-detect';
 
-ReactDOM.render(<AppDesktop />, document.getElementById('root'));
+const md = new MobileDetect(window.navigator.userAgent);
+
+ReactDOM.render(
+  md.mobile() ? <AppMobile /> : <AppDesktop />, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
