3d5c5dbe8acd2d1428db3afefac43efeffe72e91
Patat <nemnogosloven@gmail.com>
2018-11-13 18:20:06 +0300

    refactor: EventCard_type_critical

--- client-bem/src/common.blocks/EventCard/_type/EventCard_type_critical.tsx
+++ client-bem/src/common.blocks/EventCard/_type/EventCard_type_critical.tsx
@@ -1,5 +1,6 @@
 import { withBemMod } from '@bem-react/core';
 import { IEventCardProps } from '../index';
+import './EventCard_type_critical.css';
 
 export const EventCardTypeCritical = withBemMod<IEventCardProps>(
   'EventCard',
